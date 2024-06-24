ad802f3879e631335b0a0b72e5499537d1d4f0a4
xcc <yangjun2001x@163.com>
2024-06-24 21:46:54 +0800

    update

--- devel/lib/python3/dist-packages/dlio_loc/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from ._cloud_info import *
-from ._state_info import *
--- devel/share/dlio_loc/cmake/dlio_loc-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(dlio_loc_MSG_INCLUDE_DIRS "/home/xc/loc_ws/src/lio_loc/msg")
-set(dlio_loc_MSG_DEPENDENCIES sensor_msgs;std_msgs;geometry_msgs;nav_msgs)
